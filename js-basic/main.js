3ca16e7fe8b3a0b3d84cdfd0c6de3ec0a81f742f
masahito-s <ec2-user@ip-172-31-44-132.ec2.internal>
2020-10-21 14:05:48 +0000

    JavaScript-START

--- js-basic/main.js
+++ js-basic/main.js
@@ -1,1 +1,30 @@
-alert('こんにちは！')
+// alert('こんにちは！');
+// alert(111 + 222);
+// その他のダイアログ confirm(OK/cansel) prompt(入力を受け取る)
+// console.log('こんにちは！');
+// console.log('JavaScriptの世界にようこそ！');
+// let name = '太郎さん';
+// console.log(name);
+// console.log(name);
+// name = '花子さん';
+// console.log(name);
+// const name = '太郎さん';
+// console.log(name);
+// console.log(name);
+// name ='花子さん';
+// console.log(name);
+// console.log("Hello World!");
+// シングルクォーテーションとダブルクォーテーションをそれぞれ文字列として出力する
+// console.log("Hello ' World!");
+// console.log('Hello " World!');
+//   \n  で改行
+// console.log('Hello \' World!');
+// console.log("123\n456\n789");
+// console.log('こんにちは、'+'太郎さん');
+// console.log('111'+222);
+/*const name = '太郎';
+const hello = 'こんにちは、' + name + 'さん';
+console.log(hello);
+const goodEvening = 'こんばんは、' + name + 'さん';
+console.log(goodEvening);*/
+
---
+++ memo.js
@@ -0,0 +1,3 @@
+alert('こんにちは！');
+// alert(111 + 222);
+// その他のダイアログ confirm(OK/cansel) prompt(入力を受け取る)
