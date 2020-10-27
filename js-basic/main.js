a95c393f676b0bb28f1019bd5a5633e202113e20
masahito-s <ec2-user@ip-172-31-44-132.ec2.internal>
2020-10-27 19:02:48 +0000

    lesson-js-4-2

--- js-basic/main.js
+++ js-basic/main.js
@@ -1,30 +1,64 @@
 // alert('こんにちは！');
+
 // alert(111 + 222);
+
 // その他のダイアログ confirm(OK/cansel) prompt(入力を受け取る)
+
 // console.log('こんにちは！');
 // console.log('JavaScriptの世界にようこそ！');
+
 // let name = '太郎さん';
 // console.log(name);
 // console.log(name);
 // name = '花子さん';
 // console.log(name);
+
 // const name = '太郎さん';
 // console.log(name);
 // console.log(name);
 // name ='花子さん';
 // console.log(name);
+
 // console.log("Hello World!");
 // シングルクォーテーションとダブルクォーテーションをそれぞれ文字列として出力する
 // console.log("Hello ' World!");
 // console.log('Hello " World!');
+
 //   \n  で改行
 // console.log('Hello \' World!');
 // console.log("123\n456\n789");
 // console.log('こんにちは、'+'太郎さん');
 // console.log('111'+222);
+
 /*const name = '太郎';
 const hello = 'こんにちは、' + name + 'さん';
 console.log(hello);
 const goodEvening = 'こんばんは、' + name + 'さん';
 console.log(goodEvening);*/
 
+/*const numbers =`123
+456
+789`;
+console.log(numbers);
+
+const name = '太郎';
+const hello = `こんにちは、${name}さん`;
+console.log(hello);*/
+
+/*
+function helloConsole() { //関数を定義する
+  console.log('こんにちは！');
+}
+helloConsole();//関数を呼び出す
+
+function helloConsole() { //関数を呼び出す
+  console.log('こんにちは！');
+}
+helloConsole();
+helloConsole();
+*/
+
+function helloConsole(name) { //関数を定義する
+  console.log(`こんにちは、${name}さん！`);
+}
+helloConsole('太郎'); //関数を呼び出す
