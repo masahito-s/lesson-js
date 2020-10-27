d6e511917caf6a30bbbeee1119f17f658b56ff95
masahito-s <ec2-user@ip-172-31-44-132.ec2.internal>
2020-10-27 20:50:21 +0000

    4-6関数finish

--- js-basic/main.js
+++ js-basic/main.js
@@ -57,8 +57,84 @@ function helloConsole() { //関数を呼び出す
 helloConsole();
 helloConsole();
 */
-
+/*
 function helloConsole(name) { //関数を定義する
   console.log(`こんにちは、${name}さん！`);
 }
 helloConsole('太郎'); //関数を呼び出す
+*/
+/*
+function greeting() {
+  const hour = new Date().getHours(); //現在の時刻を取得
+  if( hour  <= 11 ) {
+        return 'おはようございます。'; //午前中なので、朝の挨拶を返す
+    }
+    return 'こんにちは。'; //午後(１２時以降)なので、お昼の挨拶を返す
+}
+
+console.log(greeting()); //関数を呼び出す
+*/
+
+/*  戻り値を返す関数
+function greeting() {
+    const hour = new Date().getHours(); //現在の時刻を取得
+    if( hour <= 11 ) {
+        return 'おはようございます。';
+    }
+    return 'こんにちは。';
+}
+
+const aisatsu = greeting();
+
+console.log('太郎さん、' + aisatsu); //太郎さんという文字列と変数の値をコンソールに表示する
+console.log('花子さん、' + aisatsu); //花子さんという文字列と変数の値をコンソールに表示する
+*/
+
+/*　引数を受け取り、戻り値を返す関数（正方形）
+function findSquareArea(length) { //関数を定義する
+  return length * length //引数を２乗した値を、戻り値として返す
+}
+const result = findSquareArea(5); //戻り値をresult変数に代入する
+console.log(result); //result変数の値を、コンソールに表示する
+
+　　//上記の関数は下記のようにも書ける
+　　
+function findSquareArea(length) { //関数を定義する
+  const area = length * length; //引数を２乗した値を、area変数に代入する
+  return area; //area変数の値を、戻り値として返す
+}
+const result = findSquareArea(5); //戻り値をresult変数に代入する
+console.log(result); //result変数の値を、コンソールに表示する
+*/
+
+/*　引数を2つ使い、2つの値を足算する関数
+function add(x,y) { //関数を定義する
+  const sum = x + y; //引数を足算した値を、sum変数に代入する
+  return sum; // sum変数の値を、戻り値として返す
+}
+const result = add(2,3); //戻り値をresult変数に代入する
+console.log(result); //result変数の値を、コンソールに表示する
+*/
+/* 4-6関数　練習問題　三角形の面積を求める
+function findTriangleArea(x,y,z) { //関数を定義
+    const sum = x * y / z; //引数を計算した値を、sam変数に代入する
+    return sum; //sum変数の値を、戻り値として返す
+}
+const result = findTriangleArea(5,2,2); //戻り値をresult変数に代入する
+console.log(result); // result変数の値を、コンソールに表示する
+
+　　三角形の面積を求めるので、高さと長さの数値だけで良かった。下記解答例
+　　
+function findTriangleArea(a,b) {
+    return a * b / 2;
+}
+const area = findTriangleArea(5,2);
+console.log(area);
+*/
+//　無名関数　上の正方形のやつを書き換えてみる
+const add = function (x,y) {
+    const sum = x + y;
+    return sum;
+}
+const result = add(2,3);
+console.log(result);
